0303cdc5d5df7bab97e9d944aa32ab89cd0366ac
dorogakmillionuru-spec <dorogakmillionu.ru@gmail.com>
2026-04-13 21:40:00 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const sessions = {};
 export default async function handler(req, res) {
   if (req.method !== "POST") {
