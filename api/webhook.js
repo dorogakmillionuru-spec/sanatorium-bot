919a8a64e12d348773ecc3b5b5eea3320dc4ecb2
dorogakmillionuru-spec <dorogakmillionu.ru@gmail.com>
2026-04-20 23:56:19 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
 
     await sendTelegramMessage(
       chatId,
-      "Оплата прошла ✅\nДоступ открыт.\nМожешь возвращаться в бот и продолжать."
+  text: "Вижу, оплата прошла ✅\nТеперь можно выдохнуть.\nЧто накопилось?"
     );
 
     return res.status(200).send("ok");
